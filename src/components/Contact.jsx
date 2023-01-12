5825a16cb51fe70e4a9d32322e782f2d49c04a9b
Rodriix99 <roescal347@gmail.com>
2023-01-12 12:00:58 -0300

    delay

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -102,7 +102,7 @@ function Contact() {
       animate={{ opacity: 1 }}
       transition={{
         duration: 0.8,
-        delay: 0.5,
+        // delay: 0.5,
         ease: [0, 0.71, 0.2, 1.01],
       }}
       style={{
